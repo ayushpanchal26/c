9e95ccfb894fd49e7a2584018fd3dec754d0649b
ayushpanchal2605 <ayushpanchal2605@gmail.com>
2008-06-17 00:40:33 +0530

    reverse function

---
+++ first.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+ void reverse(int arr[], int n);
+ void printArr(int arr[], int n);
+
+ int main(){
+    int arr[]={1,2,3,4,5};
+    reverse(arr, 5);
+    printArr(arr,5);
+    return 0;
+ }
+
+ void printArr(int arr[], int n){
+    for (int i = 0 ; i<n; i++){
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+    }
+
+void reverse(int arr[], int n){
+   for (int i = 0 ; i<n/2; i++){
+    int firstVal = arr[i];
+    int secondVal = arr[n - i-1];
+    arr[i]= secondVal;
+    arr[n-i-1]= firstVal;
+   }
+}
+ 
