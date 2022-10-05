e7416aad2b0864784e54ecb0b0b239b59b12e397
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-05 21:15:39 +0530

    Create ternary.C

---
+++ ternary.C
@@ -0,0 +1,12 @@
+// Ternary operator 
+#include<stdio.h>
+
+int main(){
+    int age;
+    printf("Enter age");
+    scanf("%d", &age);
+
+    age>=18?printf("adult\n"):printf("not adult");
+    
+return 0;
+}
