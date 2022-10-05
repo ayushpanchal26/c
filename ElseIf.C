41cf16486d35df51d32ea391874daff281dccae4
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-05 21:11:33 +0530

    Add files via upload

---
+++ ElseIf.C
@@ -0,0 +1,38 @@
+//  example Else If.
+
+#include<stdio.h>
+
+  int main(){
+
+    
+          int age;
+  
+          printf("Enter age");
+ 
+          scanf("%d", &age);
+    
+  
+        
+          if(age>=18){
+  
+                printf("Adult\n");
+ 
+           }
+   
+ 
+          else if(age>13 && age<18){
+ 
+                    printf("Teenager\n");
+ 
+           }
+  
+          else {
+     
+                    printf("child");
+  
+  }
+
+
+return 0;
+
+}
