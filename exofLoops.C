6d3bfcf3124875288de6eb39af9af5698e6637f3
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-07 10:39:21 +0530

    Create exofLoops.C

---
+++ exofLoops.C
@@ -0,0 +1,19 @@
+// Program to print the sum of first n natural numbers.(with printing them in reverse).
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    
+    int sum=0;
+    for(int i=1;i<=n;i++){
+    sum=sum+i; //sum+=i
+    }
+    printf("sum is %d\n",sum);
+    
+    for(int i=n; i>=1; i--){
+        printf("%d\n",i);
+    }
+return 0;
+}
