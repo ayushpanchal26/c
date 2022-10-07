d7bb7c4a4186bfee121a99b80109cff35d96e6d9
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-07 10:23:31 +0530

    Create exofWhileloop.C

---
+++ exofWhileloop.C
@@ -0,0 +1,15 @@
+// examples of while loop.
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter number:");
+    scanf("%d",&n);
+    
+    int i=0;
+    while(i<=n){
+        printf("%d\n",i);
+        i++;
+    }
+return 0;
+}
