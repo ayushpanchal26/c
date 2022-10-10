cefdb0674ad7cdf36d5d6b27b637bb76ac32f3a2
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-10 12:05:45 +0530

    Create odd.C

---
+++ odd.C
@@ -0,0 +1,15 @@
+// print all the number from 5to50.
+#include<stdio.h>
+
+int main(){
+    for(int i=5; i<=50; i++){
+    if(i%2 !=0){
+        printf("%d\n",i);
+    }//odd
+    }
+    return 0;
+}
+
+// through these program we can short-list the items from the group for example is a company visits a college for the placemnts and there are 300 students.
+// than firstly we will take the students data secondly the company will decide the CG they want let belive that the compamy wants 8.00 so we will put that number
+// in this program and the prgram will short-list students for the interview.
