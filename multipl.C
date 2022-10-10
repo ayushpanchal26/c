80e1ee4e4e28c0738cd1cfbf8cbcec1b5211f1c2
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-10 11:34:11 +0530

    Create multipl.C

---
+++ multipl.C
@@ -0,0 +1,17 @@
+// Keep talking number from the user until the user enters a number which is mutliple of 7.
+#include<stdio.h>
+
+int main(){
+    printf("Enter a number:")
+    scanf("%d", &n);
+    printf("%d\n",n);
+    
+    if(n%7 !=0){ //multiple of 7
+        break;
+    }
+    }while(!);
+    printf("thank you\n")
+return 0;
+}
+
+// If the user input any integer the program will give an output on that input and if the number enterted by the user is a multiple of 7 the program will stop working and it will show thank you but if the input is not a multiple of 7 than the loop will work continously.
