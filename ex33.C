d7f1503421a49d02014c9e734f7a1327d4b625fa
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-12 21:34:04 +0530

    Create ex33.C

---
+++ ex33.C
@@ -0,0 +1,17 @@
+// print the factorial of N
+#include<stdio.h>
+
+int fact(int n);
+int main(){
+    printf("factorial is: %d", fact(6));
+    return 0;
+}
+
+int fact(int n){
+    if(n == 0){
+        return 1;
+    }
+    int factNm1 = fact(n-1);
+    int factN = factNm1*n;
+    return factN;
+}
