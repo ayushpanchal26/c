e2018774a418c4feba42e2779823aa4e54106328
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-12 20:42:05 +0530

    Create ex27.C

---
+++ ex27.C
@@ -0,0 +1,18 @@
+// functions to print "hello"&second to print"good bye",[example of working of the function]
+#include<stdio.h>
+void printHello();
+void printGoodbye();
+
+int main(){
+       printHello();
+       printGoodbye();
+       return 0;
+}
+
+void printhello(){
+    printf("hello\n");
+}
+void printGoodbye(){
+      printf("goodbye:\n");
+}
+   
