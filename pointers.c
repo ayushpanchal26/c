0a99196419740a8c19bc5aa96c78c014e2066432
ayushpanchal2605 <ayushpanchal2605@gmail.com>
2022-10-18 11:24:06 +0530

    fix the print statement

---
+++ pointers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void square(int n);
+void _square(int*n);
+int main(){
+    int number = 4;
+    square(number);
+    printf("number = %d\n", number);
+    // return 0;
+}
+
+// call by value
+void square(int n){
+    n = n*n;
+    printf("square = %d\n",n);
+}
+
+void _square(int*n){
+    *n = (*n)*(*n);
+    printf("square = %d\n",*n);
+}
