a3472c47c8aaa4d310c084fc3d12243753b8cffc
ayushpanchal2605 <ayushpanchal2605@gmail.com>
2022-10-18 11:52:59 +0530

    arrayexamples

---
+++ exc.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+  
+   int  marks[3];
+   printf("Enter Physcis: ");
+   scanf ("%d",&marks[0]);
+    
+   printf("Enter chemsitry:");
+   scanf("%d", &marks[1]);
+
+   printf("Enter  Math:");
+   scanf("%d", &marks[2]);
+    
+   printf("Physcis = %d , chem = %d ,  math = %d ",  marks[1], marks[1] ,marks[2]);
+}
+
+// these function basically store the data in memory of the computer.
