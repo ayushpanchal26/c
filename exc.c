2d95f04934db8f55e93201ef1cd282d9ba82158f
ayushpanchal2605 <ayushpanchal2605@gmail.com>
2022-11-03 18:09:42 +0530

    xyz

--- exc.c
+++ exc.c
@@ -4,15 +4,15 @@ int main(){
   
    int  marks[3];
    printf("Enter Physcis: ");
-   scanf ("%d",&marks[0]);
+   scanf ("%d\n", &marks[0]);
     
-   printf("Enter chemsitry:");
-   scanf("%d", &marks[1]);
-
+   printf("Enoter chemsitry:");
+   scanf("%d\n", &marks[1]);
+ 
    printf("Enter  Math:");
-   scanf("%d", &marks[2]);
+   scanf("%d\n", &marks[2]);
     
-   printf("Physcis = %d , chem = %d ,  math = %d ",  marks[1], marks[1] ,marks[2]);
+   printf("Physcis = %d,chem = %d,math=%d",marks[1], marks[1] ,marks[2]);
 }
 
 // these function basically store the data in memory of the computer.
