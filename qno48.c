3d149604acd93a79a9a201edcdc0a7e8190a43a1
ayushpanchal2605 <ayushpanchal2605@gmail.com>
2022-11-11 22:22:55 +0530

    questions on string functions

---
+++ qno48.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    char firstname[100];
+    scanf("%s",firstname);
+    printf("your name is %s", firstname);
+
+    return 0;
+}
+
+
+// example of %s (string fromat specifer)
