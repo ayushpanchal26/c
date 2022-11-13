514a5308379477301f2a7b02416efd75f6f4f67d
ayushpanchal2605 <ayushpanchal2605@gmail.com>
2022-11-13 10:17:19 +0530

    strings by pointer

---
+++ stringbypointers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char * canchange = "hii";
+    puts(canchange);
+
+    canchange = "hello world";
+    puts(canchange);
+
+     return  0 ;
+}
+
+// we can modify charcter using pointers.
